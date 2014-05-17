54bb86048d400b3ee2bb86ad8f75729c54e116a5
Gea-Suan Lin <gslin@gslin.org>
2014-05-18 06:22:34 +0800

    Install elasticsearch.

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+work() {
+    wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
+    sed -i -e 's@http://archive.ubuntu.com@http://tw.archive.ubuntu.com@' /etc/apt/sources.list
+
+    cat > /etc/apt/sources.list.d/elasticsearch.list <<EOF
+deb http://packages.elasticsearch.org/elasticsearch/1.1/debian stable main
+EOF
+
+    apt-get update
+
+    DEBIAN_FRONTEND=noninteractive apt-get -y install git vim-nox
+    DEBIAN_FRONTEND=noninteractive apt-get -y install default-jdk elasticsearch
+
+    update-rc.d elasticsearch defaults 95 1
+
+    service elasticsearch start
+}
+
+work
